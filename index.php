c058825b88fd1b7743107c45106c684c8c424844
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-16 15:10:04 +0300

    f

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	$chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
 	$name = $result["message"]["from"]["first_name"]; //Юзернейм пользователя
 	$keyboard = [["Последние статьи"],["Картинка"],["Гифка"]]; //Клавиатура
+	$url = 'http://vin-car1.a0001.net/api.php?nomer=';
 	if($text)
 	{
 		if($text == "/start") 
@@ -15,35 +16,43 @@
 			$reply = $name . ", Добро пожаловать в бота! ";
 			$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply]);
 		}
+		else
+		{
+			$number = $text;
+			$data = curl_init();
+			curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($data, CURLOPT_URL, $url . $number);
+			$response = curl_exec($data);
+			curl_close($data);
+			$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $response]);
+			// $reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
+			// $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply]);
+		}
 		if ($text == "/help") 
 		{
 			$reply = "Информация с помощью.";
 			$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
 		}
-		if ($text == "Картинка") 
-		{
-			$url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
-			$telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
-		}
-		if ($text == "Гифка") 
-		{
-		    $url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
-		    $telegram->sendDocument([ 'chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание." ]);
-		}
-		if ($text == "Последние статьи")
-		{
-			$html=simplexml_load_file('http://netology.ru/blog/rss.xml');
-			foreach ($html->channel->item as $item) 
-			{
-			    $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
-			}
-			$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
-		}
-		else
-		{
-			$reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
-			$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply]);
-		}
+		// if ($text == "Картинка") 
+		// {
+		// 	$url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
+		// 	$telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
+		// }
+		// if ($text == "Гифка") 
+		// {
+		//     $url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
+		//     $telegram->sendDocument([ 'chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание." ]);
+		// }
+		// if ($text == "Последние статьи")
+		// {
+		// 	$html=simplexml_load_file('http://netology.ru/blog/rss.xml');
+		// 	foreach ($html->channel->item as $item) 
+		// 	{
+		// 	    $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
+		// 	}
+		// 	$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
+		// }
+
 	}
 	else
 	{

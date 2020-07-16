d8e8a73cfa8a922b343c3400f2a595f4e98a5b55
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-16 13:00:54 +0300

    better

--- index.php
+++ index.php
@@ -14,27 +14,32 @@
 		{
 			$reply = $name . ", Добро пожаловать в бота! ";
 			$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply]);
-		}elseif ($text == "/help") 
-		{
-		$reply = "Информация с помощью.";
-		$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-		}elseif ($text == "Картинка") 
-		{
-		$url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
-		$telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
-		}elseif ($text == "Гифка") 
-		{
-		$url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
-		$telegram->sendDocument([ 'chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание." ]);
-		}elseif ($text == "Последние статьи")
-		{
-		$html=simplexml_load_file('http://netology.ru/blog/rss.xml');
-		foreach ($html->channel->item as $item) 
-		{
-		    $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
 		}
-		$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
-		}else
+		if ($text == "/help") 
+		{
+			$reply = "Информация с помощью.";
+			$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+		}
+		if ($text == "Картинка") 
+		{
+			$url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
+			$telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
+		}
+		if ($text == "Гифка") 
+		{
+		    $url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
+		    $telegram->sendDocument([ 'chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание." ]);
+		}
+		if ($text == "Последние статьи")
+		{
+			$html=simplexml_load_file('http://netology.ru/blog/rss.xml');
+			foreach ($html->channel->item as $item) 
+			{
+			    $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
+			}
+			$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
+		}
+		else
 		{
 			$reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
 			$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply]);

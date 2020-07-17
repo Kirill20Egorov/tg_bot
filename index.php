a2a68007e68d5e685e7b62ed332c008e07637b19
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-17 14:36:17 +0300

    f

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 	$name = $result["message"]["from"]["first_name"]; //Юзернейм пользователя
 	if($text)
 	{
-		$reply = "Menu:";
-		$reply_markup = $telegram->replyKeyboardMarkUp([ 'keyboard' => menu, 'resize_keyboard' => true, 'one_time_keyboard' => false]);
-		$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'reply_markup' => $reply_markup]);;
+		// $reply = "Menu:";
+		// $reply_markup = $telegram->replyKeyboardMarkUp([ 'keyboard' => menu, 'resize_keyboard' => true, 'one_time_keyboard' => false]);
+		// $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'reply' => $reply, 'reply_markup' => $reply_markup]);
 		if($text == "/start") 
 		{
 

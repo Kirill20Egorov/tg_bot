34ad620925126a8f8bd6ecdd2ca81de3e0953967
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-17 14:40:58 +0300

    а

--- index.php
+++ index.php
@@ -1,21 +1,17 @@
 <?php
 
 	include('vendor/autoload.php'); //Подключаем библиотеку
-    include('menu.php');
- 	use Telegram\Bot\Api;
+	use Telegram\Bot\Api;
 	$telegram = new Api('1234407965:AAEgvF_OTn7A0KutIWRTzfiX2AhKTfaSXC4'); //Устанавливаем токен, полученный у BotFather
 	$result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
 	$text = $result["message"]["text"]; //Текст сообщения
 	$chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
 	$name = $result["message"]["from"]["first_name"]; //Юзернейм пользователя
+	$keyboard = [["Последние статьи"],["Картинка"],["Гифка"]]; //Клавиатура
 	if($text)
 	{
-		$reply = "Menu:";
-		$reply_markup = $telegram->replyKeyboardMarkUp([ 'keyboard' => menu, 'resize_keyboard' => true, 'one_time_keyboard' => false]);
-		$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'reply' => $reply, 'reply_markup' => $reply_markup]);
 		if($text == "/start") 
 		{
-
 			$reply = $name . ", Добро пожаловать в бота! ";
 			$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply]);
 		}
@@ -28,11 +24,11 @@
 	            var_dump($url);
 			    $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $url]);
                 $obj = json_decode($url);
-                $key = $obj -> key;
+                $email = $obj -> email;
 
 			// $reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
 			// $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply]);
-			}			
+			}
 		}
 		if ($text == "/help") 
 		{
@@ -58,10 +54,6 @@
 		// 	}
 		// 	$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
 		// }
-		else
-		{
-			    $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $text]);
-		}
 
 	}
 	else

098f1f3a4c6852d3a95a5d427c50fa72fb94e50e
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-17 14:45:55 +0300

    f

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 	include('vendor/autoload.php'); //Подключаем библиотеку
+	include('menu.php');
 	use Telegram\Bot\Api;
 	$telegram = new Api('1234407965:AAEgvF_OTn7A0KutIWRTzfiX2AhKTfaSXC4'); //Устанавливаем токен, полученный у BotFather
 	$result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
@@ -14,6 +15,9 @@
 		{
 			$reply = $name . ", Добро пожаловать в бота! ";
 			$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply]);
+			$reply = "Menu: ";
+			$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $menu, 'resize_keyboard' => true, 'one_time_keyboard' => false]);
+			$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
 		}
 		else
 		{
@@ -35,26 +39,6 @@
 			$reply = "Информация с помощью.";
 			$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
 		}
-		// if ($text == "Картинка") 
-		// {
-		// 	$url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
-		// 	$telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
-		// }
-		// if ($text == "Гифка") 
-		// {
-		//     $url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
-		//     $telegram->sendDocument([ 'chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание." ]);
-		// }
-		// if ($text == "Последние статьи")
-		// {
-		// 	$html=simplexml_load_file('http://netology.ru/blog/rss.xml');
-		// 	foreach ($html->channel->item as $item) 
-		// 	{
-		// 	    $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
-		// 	}
-		// 	$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
-		// }
-
 	}
 	else
 	{

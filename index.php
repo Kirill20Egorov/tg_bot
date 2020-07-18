9063d005cbf58f284e74ee49f2da9f060bb162e1
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-18 21:40:46 +0300

    f

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 	include('vendor/autoload.php'); //Подключаем библиотеку
+	include('menu.php');
 	use Telegram\Bot\Api;
 	$telegram = new Api('1234407965:AAEgvF_OTn7A0KutIWRTzfiX2AhKTfaSXC4'); //Устанавливаем токен, полученный у BotFather
 	$result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
@@ -13,6 +14,9 @@
 		{
 			$reply = $name . ", Добро пожаловать в бота! ";
 			$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply]);
+			$reply = "Menu: ";
+			$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $menu, 'resize_keyboard' => true, 'one_time_keyboard' => false]);
+			$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
 		}
 		else
 		{
@@ -22,7 +26,6 @@
                 $text = $url;
 	            var_dump($url);
 			    $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $url]);
-			    $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => 'Сообщение']);
                 $obj = json_decode($url);
                 $email = $obj -> email;
 

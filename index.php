c8fa544c0f14adca664b3f8d2f415c87e770ede7
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-19 22:25:29 +0300

    а

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php
 
 	include('vendor/autoload.php'); //Подключаем библиотеку
-	include('menu.php');
 	use Telegram\Bot\Api;
 	$telegram = new Api('1234407965:AAEgvF_OTn7A0KutIWRTzfiX2AhKTfaSXC4'); //Устанавливаем токен, полученный у BotFather
 	$result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
 	$text = $result["message"]["text"]; //Текст сообщения
 	$chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
 	$name = $result["message"]["from"]["first_name"]; //Юзернейм пользователя
+	$menu = [['Привет','Создать почту'], ['Жанр3', 'Жанр4']];
 	if($text)
 	{
-		$reply = 'Привет';
-		$reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $menu, 'resize_keyboard' => true, 'one_time_keyboard' => true]);
-		$telegram->sendMessage([ 'chat_id' => $chat_id,'text' => $reply, 'reply_markup' => $reply_markup]);
-		
+		if ($text == "меню")
+		{
+			$reply = 'Привет';
+			$reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $menu, 'resize_keyboard' => true, 'one_time_keyboard' => true]);
+			$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
+		}
+
 		if($text == "/start") 
 		{
 			$reply = $name . ", Добро пожаловать в бота! ";

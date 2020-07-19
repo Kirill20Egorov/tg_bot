ac53022193d47776bb1132619ef109ff63935e90
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-19 22:11:05 +0300

    f

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 	$name = $result["message"]["from"]["first_name"]; //Юзернейм пользователя
 	if($text)
 	{
+		$reply = 'Привет';
+		$reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $menu, 'resize_keyboard' => true, 'one_time_keyboard' => true]);
+		$telegram->sendMessage([ 'chat_id' => $chat_id,'text' => $reply, 'reply_markup' => $reply_markup]);
+		
 		if($text == "/start") 
 		{
 			$reply = $name . ", Добро пожаловать в бота! ";

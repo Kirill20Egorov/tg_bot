acc2d5948f2b5e5af7f3aa2aa37f180fc31f0939
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-20 23:30:23 +0300

    f

--- index.php
+++ index.php
@@ -12,9 +12,52 @@
 	{
 		if ($text == "menu")
 		{
-			$reply = "Выберите подходящий жарн";
-			$reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $menu, 'resize_keyboard' => true, 'one_time_keyboard' => true]);
-			$telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
+use Telegram\Bot\Commands\Command;
+use Telegram\Bot\Keyboard\Keyboard;
+
+$telegram = new Telegram\Bot\Api('MY_KEY'); 
+        
+$update = $telegram->getWebhookUpdates();
+
+// данные сообщения в зависимости от callback_query
+			if ( isset($this->update['callback_query'])) {
+			    $message = $update['callback_query'];
+			} else {
+			    $message = $update;
+			}
+
+			$chatId = $message['message']['chat']['id'];
+
+			// правильно формируем клавиатуру:
+			$keyboard = [
+			    [
+			        Keyboard::inlineButton(['callback_data'=>'/butt1','text'=>'Кнопка 1']),
+			        Keyboard::inlineButton(['callback_data'=>'/buut2','text'=>'Кнопка 2'])
+			    ]
+			];
+
+			$reply_markup = $telegram->replyKeyboardMarkup([ 
+			    // 'keyboard' => $keyboard, // вместо этого используем:
+			    'inline_keyboard' => $keyboard,
+			    'resize_keyboard' => true, 
+			    'one_time_keyboard' => false 
+			]);
+
+
+			// если нажали кнопку:
+			if ( isset($this->update['callback_query'])) {
+			  $telegram->sendMessage(array(
+			    'chat_id' => $chat_id,
+			      'text' => "Вы нажали на кнопку с кодом: " . $message['data'], // именно в $message['data'] - будет то что прописано у нажатой кнопки в качестве callback_data
+			      'reply_markup' => $reply_markup,
+			  ));
+			} else {
+			  $telegram->sendMessage(array(
+			    'chat_id' => $chat_id,
+			      'text' => 'Нажмите на одну из кнопок:',
+			      'reply_markup' => $reply_markup,
+			  ));
+			}
 
 		}
 

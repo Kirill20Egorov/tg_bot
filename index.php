6803fd08688365aaa90f0e5106809bc408d86977
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-20 12:14:25 +0300

    f

--- index.php
+++ index.php
@@ -29,8 +29,8 @@
 	            var_dump($url);
 	            $email = $obj -> email;
 	            $key = $obj -> key;
-			    $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => 'Ваш email: '  $email]);
-			    $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => 'Ваш ключ: '  $key]); 
+			    $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => 'Ваш email: ' . $email]);
+			    $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => 'Ваш ключ: ' . $key]); 
 			// $reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
 			// $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply]);
 			}

4d345330bb99821e514540b1c3cb274f9bc0db18
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-21 21:13:07 +0300

    F

--- index.php
+++ index.php
@@ -31,6 +31,25 @@
 				$key = $obj -> key;
 			    $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' =>  'Email: ' . $email]); 
 			    $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' =>  'Key: ' . $key]);
+
+	$servername = "eu-cdbr-west-03.cleardb.net";
+	$database = "heroku_c34b9131d7bdccf";
+	$username = "b0f449da77e9fd";
+	$password = "08065c02";
+	$conn = mysqli_connect($servername, $username, $password, $database);
+	// Check connection
+	if (!$conn) 
+		{
+		    die("Connection failed: " . mysqli_connect_error());
+		}
+		 
+		$sql = "DELETE * FROM users";
+		if (mysqli_query($conn, $sql)) 
+		{
+		    $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' =>  'Удалили старые']);
+		} 
+	mysqli_close($conn);
+
 			    addRecord($name, $key, $email);
 			// $reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
 			// $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply]);

d263b9ddab1ba2af11dd2d6649fb42a3c7eef4c2
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-21 15:33:17 +0300

    f

--- index.php
+++ index.php
@@ -12,26 +12,6 @@
 	// require_once('users.php');
 	if($text)
 	{
-		$servername = "eu-cdbr-west-03.cleardb.net";
-		$database = "heroku_c34b9131d7bdccf";
-		$username = "b0f449da77e9fd";
-		$password = "08065c02";
-		// Create connection
-		$conn = mysqli_connect($servername, $username, $password, $database);
-		// Check connection
-		if (!$conn) 
-		{
-		    die("Connection failed: " . mysqli_connect_error());
-		}
-		 
-		$sql = "INSERT INTO users (name, password, email) VALUES ('Thom', 'Vial', 'thom.v@some.com')";
-		if (mysqli_query($conn, $sql)) {
-		      echo "New record created successfully";
-		} else {
-		      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-		}
-		mysqli_close($conn);
-
 		$reply = "Меню";
 		$reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $menu, 'resize_keyboard' => true, 'one_time_keyboard' => true]);
 			$telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
@@ -50,14 +30,28 @@
 				$key = $obj -> key;
 			    $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' =>  'Email: ' . $email]); 
 			    $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' =>  'Key: ' . $key]);
-			// $reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
-			// $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply]);
+
+			    $servername = "eu-cdbr-west-03.cleardb.net";
+				$database = "heroku_c34b9131d7bdccf";
+				$username = "b0f449da77e9fd";
+				$password = "08065c02";
+				// Create connection
+				$conn = mysqli_connect($servername, $username, $password, $database);
+				// Check connection
+				if (!$conn) 
+				{
+				    die("Connection failed: " . mysqli_connect_error());
+				}
+				 
+				$sql = "INSERT INTO users (name, password, email) VALUES (". $name .", 'Vial', 'thom.v@some.com')";
+
+				mysqli_close($conn);
 			}
 			else
 			{
 			    if ($text == "/help") 
 	    	    {
-					$reply = "Информация с помощью.";
+					$reply = "Информация с помощью..";
 					$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
 		        }
 		        else

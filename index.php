79ee9c67c6c4b1569c1e23265cf7befcc4175ac8
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-21 19:41:52 +0300

    F

--- index.php
+++ index.php
@@ -39,10 +39,29 @@
 			{
 			    if ($text == "/help") 
 	    	    {
-					$reply = "Информация с помощью:: ";
+					$reply = "Информация с помощью. ";
 					$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-					$result = deleteRecord($result);
-					$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $result ]);
+                    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $result ]);
+                    	$servername = "eu-cdbr-west-03.cleardb.net";
+						$database = "heroku_c34b9131d7bdccf";
+						$username = "b0f449da77e9fd";
+						$password = "08065c02";
+						$conn = mysqli_connect($servername, $username, $password, $database);
+						// Check connection
+						if (!$conn) 
+						{
+						    die("Connection failed: " . mysqli_connect_error());
+						}	 
+						$sql = "DELETE FROM users WHERE name = 'sonik'";
+							if (mysqli_query($conn, $sql)) 
+							{
+							echo "New record created successfully";
+							} else 
+							{
+					        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+					        }
+						    mysqli_close($conn);
+
 
 		        }
 		        else

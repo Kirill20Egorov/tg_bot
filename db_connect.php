93e531ba448a2040c590cb6f9cb5ecca93759fab
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-23 00:33:48 +0300

    f

--- db_connect.php
+++ db_connect.php
@@ -51,19 +51,19 @@ function getKey($name)
 	return $row['password'];
 }
 
-function getChatId($id)
+function getChatId()
 {
 	$servername = "eu-cdbr-west-03.cleardb.net";
 	$database = "heroku_c34b9131d7bdccf";
 	$username = "b0f449da77e9fd";
 	$password = "08065c02";	
 	$conn = mysqli_connect($servername, $username, $password, $database);
-	// if (!$conn) 
-	// 	die("Connection failed: " . mysqli_connect_error());
-	// $sql = "SELECT chat_id FROM users";
- //    $res = mysql_query($sql);
- //    while($row = mysql_fetch_array($res))
- //    {
- //        echo "Имя:".$row['firstname']."<br>\n";
- //    }
+	if (!$conn) 
+		die("Connection failed: " . mysqli_connect_error());
+$sql = 'SELECT chat_id FROM users';
+
+$result = mysqli_query($conn, $sql);
+
+return $result;
+
 }

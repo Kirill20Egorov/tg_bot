9809a16c38776f0db022e798a2abf6341bcf2d1c
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-22 13:48:30 +0300

    f

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	$text = $result["message"]["text"]; //Текст сообщения
 	$chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
 	$name = $result["message"]["from"]["first_name"]; //Юзернейм пользователя
+	include('menu.php');
 	$menu = [['Сгенерировать почту', 'Проверить почту'], ['Прочитать письма'], ['Проверить оставшееся время']];
 	require_once('db_connect.php');
 	// require_once('db_connect.php');

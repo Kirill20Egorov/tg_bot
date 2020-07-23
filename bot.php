2b1603da121c3fc03584e24de53e9dc66d34d56d
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-23 22:24:19 +0300

    f

--- bot.php
+++ bot.php
@@ -2,11 +2,11 @@
 
 include('vendor/autoload.php'); //Подключаем библиотеку
 use Telegram\Bot\Api;
-$telegram = new Api('1234407965:AAEgvF_OTn7A0KutIWRTzfiX2AhKTfaSXC4');
-$database = "heroku_c34b9131d7bdccf";
-$username = "b0f449da77e9fd";
-$password = "08065c02";	
-$conn = mysqli_connect($servername, $username, $password, $database);
+define("SERVERNAME", "eu-cdbr-west-03.cleardb.net");
+define("DATABASE", "heroku_c34b9131d7bdccf");
+define("USERNAME", "b0f449da77e9fd");
+define("PASSWORD", "08065c02");
+$conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASE);
 if (!$conn) 
 	die("Connection failed: " . mysqli_connect_error());
 include('db_connect.php');

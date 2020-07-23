128a50bbb8683d62811ec188f2a6d2b071aa7428
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-23 12:13:53 +0300

    f

--- bot.php
+++ bot.php
@@ -4,4 +4,9 @@
 	use Telegram\Bot\Api;
 	$telegram = new Api('1234407965:AAEgvF_OTn7A0KutIWRTzfiX2AhKTfaSXC4');
 	require_once('db_connect.php');
-	$result = getChatId();
+	$result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную 
+		$result = getChatId();
+		while ($row = mysqli_fetch_array($result)) 
+		{
+		    $telegram->sendMessage([ 'chat_id' => $row['chat_id'], 'text' => 'ПРИВЕТ']);	
+		}

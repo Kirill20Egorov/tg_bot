ef0107e01296773d8acc26451e58f63c994acf79
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-23 23:05:29 +0300

    ff

--- bot.php
+++ bot.php
@@ -3,11 +3,11 @@
 include('vendor/autoload.php'); //Подключаем библиотеку
 include('index.php');
 use Telegram\Bot\Api;
-define("SERVERNAME", "eu-cdbr-west-03.cleardb.net");
-define("DATABASE", "heroku_c34b9131d7bdccf");
-define("USERNAME", "b0f449da77e9fd");
-define("PASSWORD", "08065c02");
-$conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASE);
+$SERVERNAME = "eu-cdbr-west-03.cleardb.net");
+$DATABASE = "heroku_c34b9131d7bdccf");
+$USERNAME = "b0f449da77e9fd");
+$PASSWORD = "08065c02");
+$conn = mysqli_connect($SERVERNAME, $USERNAME, $PASSWORD, $DATABASE);
 if (!$conn) 
 	die("Connection failed: " . mysqli_connect_error());
 include('db_connect.php');
--- index.php
+++ index.php
@@ -13,6 +13,7 @@ define("SERVERNAME", "eu-cdbr-west-03.cleardb.net");
 define("DATABASE", "heroku_c34b9131d7bdccf");
 define("USERNAME", "b0f449da77e9fd");
 define("PASSWORD", "08065c02");
+define("URL", "https://post-shift.ru/api.php?action=")
 $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASE);
 if (!$conn) 
 	die("Connection failed: " . mysqli_connect_error());
@@ -25,7 +26,7 @@ switch($text)
 		break;
 	case '/email':
 	case 'Сгенерировать почту':
-		$url =  file_get_contents("https://post-shift.ru/api.php?action=new&type=json");
+		$url =  file_get_contents(URL . "new&type=json");
 		$obj = json_decode($url);
 		$email =  $obj -> email;
 		$key = $obj -> key;
@@ -36,7 +37,7 @@ switch($text)
 	    break;
 	case 'Проверить почту':
 		$pass = getKey($conn, $name);
-		$url =  file_get_contents("https://post-shift.ru/api.php?action=getlist&key=" . $pass);
+		$url =  file_get_contents(URL . "getlist&key=" . $pass);
 		if ($url == 'Error: The list is empty.')
 			$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => 'Нет новых писем. Повторите позже']);		
 		else

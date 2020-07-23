99bae123001a306c9a188b004e3133bee5f8a53b
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-07-23 12:22:17 +0300

    fff

--- bot.php
+++ bot.php
@@ -3,7 +3,7 @@
 	include('vendor/autoload.php'); //Подключаем библиотеку
 	use Telegram\Bot\Api;
 	$telegram = new Api('1234407965:AAEgvF_OTn7A0KutIWRTzfiX2AhKTfaSXC4');
-	require_once('db_connect.php');
+	include('db_connect.php');
 	$result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную 
 		$result = getChatId();
 		while ($row = mysqli_fetch_array($result)) 
